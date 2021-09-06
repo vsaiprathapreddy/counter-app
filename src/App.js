a58340b716ce789eb7440c29d8ac023e63e6b84b
vsaiprathapreddy <saiprathap3424@gmail.com>
2021-09-06 11:13:47 +0530

    made an basic version

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Counter</h2>
+      <Counter label="Counter1" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,18 @@
+import React ,{ useState } from 'react'
+
+function Counter(props) {
+    const[ count,setCount ] = useState(0);
+    return (
+        <div>
+            <p>{props.label}</p>
+            <button onClick={()=>setCount(0)}>Reset</button><br />
+            <button onClick= {() => setCount(count-1)}>Decrement</button>
+            <p>{count}</p>
+            <button onClick={() => setCount(count+1)}>Increment</button>
+
+        </div>
+    )
+}
+
+export default Counter
+    
